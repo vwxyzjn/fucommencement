0ce4c648e2e0e114e391742b0cd3a77972bf5822
Unknown <vwxyzjn@foxmail.com>
2017-12-25 15:25:51 +0800

    silder worked

--- frontend/admin/pages/event.vue
+++ frontend/admin/pages/event.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
-    <nuxt-child :key="$route.params.id"></nuxt-child>
+    <slider animation="fade">
+      <slider-item v-for="(i, index) in list" :key="index">
+        <div :style="i">
+          <p style="line-height: 280px; font-size: 5rem; text-align: center;">Page{{ index + 1 }}</p>
+        </div>
+      </slider-item>
+    </slider>
   </div>
 </template>
+
+<script>
+import { Slider, SliderItem } from 'vue-easy-slider'
+
+export default {
+  data () {
+    return {
+      list: [
+        { backgroundColor: '#3f51b5', width: '100%', height: '100%' },
+        { backgroundColor: '#eee', width: '100%', height: '100%' },
+        { backgroundColor: '#f44336', width: '100%', height: '100%' }
+      ]
+    }
+  },
+  components: {
+    Slider,
+    SliderItem
+  }
+}
+</script>
+
--- frontend/admin/pages/event/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>something</div>
-</template>
