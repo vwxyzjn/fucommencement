7c10fd3e3098f2db6bc5d5acc3601ca853e4187c
Unknown <vwxyzjn@foxmail.com>
2017-12-25 16:34:10 +0800

    uninstall failed components

--- frontend/admin/pages/event.vue
+++ frontend/admin/pages/event.vue
@@ -1,32 +1,7 @@
 <template>
   <div>
-    <slider animation="fade">
-      <slider-item v-for="(i, index) in list" :key="index">
-        <div :style="i">
-          <p style="line-height: 280px; font-size: 5rem; text-align: center;">Page{{ index + 1 }}</p>
-        </div>
-      </slider-item>
-    </slider>
+    haha
   </div>
 </template>
 
-<script>
-import { Slider, SliderItem } from 'vue-easy-slider'
-
-export default {
-  data () {
-    return {
-      list: [
-        { backgroundColor: '#3f51b5', width: '100%', height: '100%' },
-        { backgroundColor: '#eee', width: '100%', height: '100%' },
-        { backgroundColor: '#f44336', width: '100%', height: '100%' }
-      ]
-    }
-  },
-  components: {
-    Slider,
-    SliderItem
-  }
-}
-</script>
 
