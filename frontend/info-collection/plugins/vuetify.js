84713e83d12660577fdb5b7c1cd2e73f2c8285c6
Costa Huang <costa.huang@outlook.com>
2018-02-10 01:05:07 -0500

    set default theme

--- frontend/info-collection/plugins/vuetify.js
+++ frontend/info-collection/plugins/vuetify.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#673ab7',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#b71c1c'
+  }
+})
