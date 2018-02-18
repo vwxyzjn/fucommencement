ec6af3686817ad8555475d4c46ac642f7499827b
Costa Huang <costa.huang@outlook.com>
2018-02-17 22:49:42 -0500

    use https

--- frontend/admin/store/index.js
+++ frontend/admin/store/index.js
@@ -1,6 +1,6 @@
 export const state = () => ({
   sidebar: false,
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'http://dev.streettraffic.org:8080',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://dev.streettraffic.org',
   indexName: process.env.NODE_ENV === 'development' ? 'student_test' : 'student'
 })
 
